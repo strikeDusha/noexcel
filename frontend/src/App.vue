4c7d92c167dfe11d2da9802c39779b7c5dd1d5a8
shustr19k <timoleonvieta@icloud.com>
2025-10-18 15:41:50 +0300

    начинаю разрабатывать саму таблицу

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="auth-container">
-    <!-- Компонент авторизации -->
-    <AuthPage v-if="currentView === 'auth'" @switch-view="switchView" />
-
-    <!-- Компонент регистрации -->
-    <RegisterPage v-else @switch-view="switchView" />
+    <component :is="currentViewComponent" @switch-view="switchView" />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import AuthPage from './components/AuthPage.vue'
 import RegisterPage from './components/RegisterPage.vue'
+import SpreadsheetPage from './components/SpreadsheetPage.vue'
 
-const currentView = ref('auth')
+// потом убрать 
+const currentView = ref('auth') // стартовая страница: авторизация
+// const currentView = ref('spreadsheet') // вместо 'auth'
+
 
 function switchView(view) {
   currentView.value = view
 }
+
+const currentViewComponent = computed(() => {
+  switch(currentView.value) {
+    case 'auth': return AuthPage
+    case 'register': return RegisterPage
+    case 'spreadsheet': return SpreadsheetPage
+  }
+})
 </script>
